6b912baadeda51b0c4401b189ece0c633955a41b
Dylan Zarn <zarndy77@gmail.com>
2024-12-22 23:23:14 -0600

    Shows a summary of a round Allows for full (course name showing) and amended versions for when showing the round in a list of all rounds vs just under a CourseDetailSheet

--- HoleOut/RoundCardView.swift
+++ HoleOut/RoundCardView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct RoundCardView: View {
+    
+    private let round: Round
+    private let course: Course
+    private let amended: Bool
+    
+    private let logger = Logger(origin: "RoundCardView")
+    
+    init(for round: Round, amended: Bool) {
+        self.round = round
+        self.course = round.course
+        self.amended = amended
+    }
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GroupBox {
+            headerSection
+            Divider()
+            statSection
+            
+        }
+        .padding(.horizontal)
+        
+    }
+    
+    // MARK: - Components
+    
+    // Shows course name as title if not amended
+    // Only shows date if amended
+    private var headerSection: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                if !amended {
+                    Label(course.name, systemImage: "flag.fill")
+                        .font(.headline)
+                        .bold()
+                }
+                if !amended {
+                    Label(round.dateString, systemImage: "calendar")
+                        .foregroundStyle(.secondary)
+                } else {
+                    Label(round.dateString, systemImage: "calendar")
+                        .font(.headline)
+                        .bold()
+                }
+                if let roundDuration = round.roundDuration {
+                    Label(roundDuration, systemImage: "clock")
+                        .foregroundStyle(.secondary)
+                }
+            }
+            Spacer()
+            RelativeScore(par: course.par, score: round.totalScore)
+                .font(.title)
+        }
+        
+    }
+    // Shows the scores for the round
+    private var statSection: some View {
+        HStack {
+            StatItem(label: "Total", value: round.totalScore)
+            Spacer()
+            StatItem(label: "Front", value: round.frontNine)
+            Spacer()
+            StatItem(label: "Back", value: round.backNine)
+        }
     }
 }
 
 #Preview {
-    RoundCardView()
+    RoundCardView(for: MockData.previewRound, amended: false)
+    RoundCardView(for: MockData.previewRound, amended: true)
 }
