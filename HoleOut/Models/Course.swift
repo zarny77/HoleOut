14da5b029150349b81d1bf1162ebf6754cb93e48
Dylan Zarn <zarndy77@gmail.com>
2024-12-22 20:19:20 -0600

    Adds computed properties for fine grained course details

--- HoleOut/Models/Course.swift
+++ HoleOut/Models/Course.swift
@@ -6,15 +6,18 @@
 //
 
 import Foundation
+import SwiftData
 
-
+@Model
 final class Course {
     // MARK: Properties
     
+    // Identifiers
     var id: UUID
     var name: String
     var address: String
     
+    // Details
     var blues: Int
     var whites: Int
     var reds: Int
@@ -32,5 +35,55 @@ final class Course {
         self.holes = holes.sorted { $0.id < $1.id }
     }
     
+    // MARK: - Computed Properties
     
+    var totalBlues: Int {
+        holes.reduce(0) { $0 + $1.blues }
+    }
+ 
+    var totalWhites: Int {
+        holes.reduce(0) { $0 + $1.whites }
+    }
+    
+    var totalReds: Int {
+        holes.reduce(0) { $0 + $1.reds }
+    }
+    
+    // Course yardage totals front 9
+    
+    var frontBlues: Int {
+        holes.prefix(9).reduce(0) { $0 + $1.blues }
+    }
+    
+    var frontWhites: Int {
+        holes.prefix(9).reduce(0) { $0 + $1.whites }
+    }
+    
+    var frontReds: Int {
+        holes.prefix(9).reduce(0) { $0 + $1.reds }
+    }
+    
+    // Course yardage totals back 9
+    
+    var backBlues: Int {
+        holes.suffix(9).reduce(0) { $0 + $1.blues }
+    }
+    
+    var backWhites: Int {
+        holes.suffix(9).reduce(0) { $0 + $1.whites }
+    }
+    
+    var backReds: Int {
+        holes.suffix(9).reduce(0) { $0 + $1.reds }
+    }
+    
+    // Par totals
+    
+    var frontPar: Int {
+        holes.prefix(9).reduce(0) { $0 + $1.par }
+    }
+    
+    var backPar: Int {
+        holes.suffix(9).reduce(0) { $0 + $1.par }
+    }
 }
