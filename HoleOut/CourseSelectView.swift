8ed6e064d303ee9cd6c442517b9d239eae67ac90
Dylan Zarn <zarndy77@gmail.com>
2024-12-22 20:17:53 -0600

    Adds the CourseSelectView Uses CourseCardViews to make a list of courses Courses are searchable by name and address

--- HoleOut/CourseSelectView.swift
+++ HoleOut/CourseSelectView.swift
@@ -6,15 +6,52 @@
 //
 
 import SwiftUI
-import SwiftData
 
 struct CourseSelectView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Round]
+    
+    @State private var searchText = ""
+    private let logger = Logger(origin: "CourseSelectView")
+    
+    // allow search by name or address
+    private var searchResults: [Course] {
+        if searchText.isEmpty {
+            return CourseRepository.shared.getAllCourses()
+        }
+        return CourseRepository.shared.courses.filter { course in
+            course.name.localizedCaseInsensitiveContains(searchText) ||
+            course.address.localizedCaseInsensitiveContains(searchText)
+        }
+    }
 
     var body: some View {
-        Text("CourseSelectView")
+        NavigationStack {
+            Group {
+                if searchResults.isEmpty {
+                    VStack {
+                        Spacer()
+                        ContentUnavailableView(
+                            "No courses matching \(searchText)",
+                            systemImage: "flag.slash",
+                            description: Text("Try a different course maybe?")
+                        )
+                        Spacer()
+                    }
+                } else {
+                    ScrollView {
+                        LazyVStack {
+                            ForEach(searchResults) { course in
+                                CourseCardView(for: course)
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("Select Course")
+            .searchable(text: $searchText, prompt: "Search courses")
+        }
     }
+
 }
 
 #Preview {
