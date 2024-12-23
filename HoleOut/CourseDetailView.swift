09302d5163aaea03d58dab61f60235bbbc9f621b
Dylan Zarn <zarndy77@gmail.com>
2024-12-22 20:54:46 -0600

    Changes the light mode icon

---
+++ HoleOut/CourseDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseDetailView.swift
+//  HoleOut
+//
+//  Created by Dylan Zarn on 2024-12-22.
+//
+
+import SwiftUI
+
+struct CourseDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseDetailView()
+}
