45d6c66e5b98b4a15c3bd06bc967eccde4c876c6
Dylan Zarn <zarndy77@gmail.com>
2024-12-24 00:39:29 -0600

    Added scoring functionality Also added in some visual changes and transitions

--- HoleOut/Card Views/HoleScoringCard.swift
+++ HoleOut/Card Views/HoleScoringCard.swift
@@ -8,13 +8,10 @@
 import SwiftUI
 
 struct HoleScoringCard: View {
-    
+    @EnvironmentObject private var roundVM: RoundViewModel
     let hole: Hole
-//    @ObservedObject var roundVM: RoundViewModel
-    
-//    private var score: Int {
-//        roundVM.currentRound?.scores[hole.id-1] ?? hole.par
-//    }
+    @Binding var score: Int
+    private let logger = Logger(origin: "HoleScoringView")
     
     var body: some View {
         GroupBox {
@@ -24,21 +21,22 @@ struct HoleScoringCard: View {
                 buttonStack
                 scoreIndicator
             }
-            
+            .frame(height: 110)
         }
-        .padding(.horizontal)
     }
     
     // MARK: - Components
     
     private var holeDetails: some View {
         HStack{
-        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                // Hole number
                 Text(String(hole.id))
                     .font(.system(size: 50))
                     .fontWeight(.black)
                     .padding(.vertical, -5)
-            YardageMarkers(b: hole.blues, w: hole.whites, r: hole.reds)
+                // yardages
+                YardageMarkers(b: hole.blues, w: hole.whites, r: hole.reds)
             }
             Spacer()
             Image(systemName: hole.holeType.rawValue)
@@ -48,57 +46,81 @@ struct HoleScoringCard: View {
     }
     
     private var scoreIndicator: some View {
-        VStack (alignment: .trailing, spacing: -20){
-            
-            HStack {
-
-                VStack(alignment: .trailing) {
-                    RelativeScore(par: hole.par, score: 5)
-                        .font(.title)
-                }
+        
+        VStack (alignment: .trailing, spacing: -10){
+            Spacer()
+            RelativeScore(par: hole.par, score: score)
+                .font(.title)
+                .frame(width: 50, alignment: .trailing)
+                .contentTransition(.interpolate)
+                .animation(.spring(response: 0.3), value: score)
+            ZStack(alignment: .trailing) {
+                Text(String(score))
+                    .font(.system(size: 100, weight: .black))
+                    .minimumScaleFactor(0.1)
+                    .lineLimit(1)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .contentTransition(.numericText())
+                    .animation(.spring(response: 0.1), value: score)
+                    .onTapGesture {
+                        if !(roundVM.currentRound?.playedHoles[hole.id - 1] ?? true) {
+                            score = hole.par
+                        }
+                    }
             }
-            Text(String(hole.par))
-                .font(.system(size: 100, weight: .black))
+            .frame(width: 75, height: 100)
         }
-        .padding(.horizontal)
+        .frame(width: 75)
+        .padding(.leading)
     }
     
     // MARK: Buttons
     private var buttonStack: some View {
-        VStack {
-            Spacer()
-            addStroke
-            Spacer()
-            subtractStroke
-            Spacer()
+        GroupBox {
+            VStack {
+                addStroke
+                Spacer()
+                subtractStroke
+            }
         }
     }
     
     private var subtractStroke: some View {
         Button {
-            
+            if score > 1 {
+                score -= 1
+                logger.log("Decreased score on hole \(hole.id) to \(score)")
+            }
         } label: {
             Label("Subtract", systemImage: "minus.circle.fill")
                 .labelStyle(.iconOnly)
                 .font(.largeTitle)
         }
+        .symbolRenderingMode(.hierarchical)
+        .foregroundStyle(.green)
     }
     
     private var addStroke: some View {
         Button {
-            
+            if score < 15 {
+                score += 1
+                logger.log("Increased score on hole \(hole.id) to \(score)")
+            }
         } label: {
             Label("Add", systemImage: "plus.circle.fill")
                 .labelStyle(.iconOnly)
                 .font(.largeTitle)
         }
+        .symbolRenderingMode(.hierarchical)
+        .foregroundStyle(.red)
     }
 }
 
 #Preview {
     ScrollView {
         ForEach(MockData.previewCourse.holes) { hole in
-            HoleScoringCard(hole: hole)
+            let score = State(initialValue: hole.par)
+            HoleScoringCard(hole: hole, score: score.projectedValue)
         }
     }
         
